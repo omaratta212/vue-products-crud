438027ab8d88c8dd24e9cae480ca03c3b9b275df
Atta <omaratta212@gmail.com>
2020-06-24 18:59:33 +0200

    Use Form class in login

--- pages/login.vue
+++ pages/login.vue
@@ -10,36 +10,26 @@
           hammock starladder roathse. Craies vegan tousled etsy austin.
         </p>
       </div>
-      <div
+      <form
         class="lg:w-2/6 md:w-1/2 bg-gray-200 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+        @submit.prevent="onSubmit"
+        @keydown="form.errors.clear($event.target.name)"
       >
         <h2 class="text-gray-900 text-lg font-medium title-font mb-5">
           Login
         </h2>
 
-        <label class="text-base mb-2 sr-only" for="login"
-          >E-mail or username</label
-        >
-        <input
-          id="login"
-          autocomplete="email"
-          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2 mb-4"
-          name="email"
-          placeholder="E-mail or username"
-          type="text"
-        />
-        <label class="text-base mb-2 sr-only" for="password"
-          >Your password</label
-        >
-        <input
-          id="password"
-          autocomplete="current-password"
-          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2 mb-4"
-          name="password"
-          placeholder="Your password"
-          type="email"
-        />
+        <div v-for="field in form.fields" :key="field.name">
+          <LazyBaseInput
+            v-model="field.value"
+            :label="field.label"
+            :type="field.type"
+            :autocomplete="field.autocomplete"
+          />
+        </div>
+
         <button
+          type="submit"
           class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
         >
           Button
@@ -50,7 +40,41 @@
             >Create account
           </nuxt-link>
         </p>
-      </div>
+      </form>
     </div>
   </section>
 </template>
+<script>
+import Form from '~/helpers/Form'
+
+export default {
+  data() {
+    return {
+      form: new Form(
+        [
+          {
+            name: 'name',
+            type: 'text',
+            autocomplete: 'username',
+            label: 'Your username',
+            value: ''
+          },
+          {
+            name: 'email',
+            type: 'email',
+            autocomplete: 'email',
+            label: 'Email address',
+            value: ''
+          }
+        ],
+        this.$axios
+      )
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.form.post('/login').then((response) => alert('Done!'))
+    }
+  }
+}
+</script>
