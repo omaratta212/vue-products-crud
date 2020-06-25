7fd72da38aba790a9767929a0fdd41c14355136b
Omar Atta <omaratta212@gmail.com>
2020-06-25 08:27:45 +0200

    DRY Form handling (#10)
    * Create BaseInput component
    
    * Add axios config
    
    * Add form handler class & transform registration form
    
    * Use Form class in login
    
    * Handle laravel validator errors
    
    * Add genneral errors alert
    
    * Finish form forntend validation
    
    * Disable button if there are errors

--- pages/login.vue
+++ pages/login.vue
@@ -10,39 +10,34 @@
           hammock starladder roathse. Craies vegan tousled etsy austin.
         </p>
       </div>
-      <div
+      <form
         class="lg:w-2/6 md:w-1/2 bg-gray-200 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+        @submit.prevent="onSubmit"
+        @keydown="form.errors.clear($event.target.name)"
       >
         <h2 class="text-gray-900 text-lg font-medium title-font mb-5">
           Login
         </h2>
 
-        <label class="text-base mb-2 sr-only" for="login"
-          >E-mail or username</label
-        >
-        <input
-          id="login"
-          autocomplete="email"
-          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2 mb-4"
-          name="email"
-          placeholder="E-mail or username"
-          type="text"
-        />
-        <label class="text-base mb-2 sr-only" for="password"
-          >Your password</label
-        >
-        <input
-          id="password"
-          autocomplete="current-password"
-          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2 mb-4"
-          name="password"
-          placeholder="Your password"
-          type="email"
-        />
+        <div v-for="field in form.fields" :key="field.name">
+          <LazyBaseInput
+            v-model="field.value"
+            :name="field.name"
+            :label="field.label"
+            :type="field.type"
+            :autocomplete="field.autocomplete"
+            :error="form.errors.get(field.name)"
+            @blur="form.validate(field)"
+          />
+        </div>
+
         <button
+          type="submit"
           class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
+          :class="{ 'opacity-50 cursor-not-allowed': form.errors.any() }"
+          :disabled="form.errors.any()"
         >
-          Button
+          Login Now
         </button>
         <p class="text-sm text-gray-500 mt-3">
           Don't have an account?
@@ -50,7 +45,51 @@
             >Create account
           </nuxt-link>
         </p>
-      </div>
+      </form>
     </div>
   </section>
 </template>
+<script>
+import Form from '~/helpers/Form'
+
+export default {
+  data() {
+    return {
+      form: new Form(
+        [
+          {
+            name: 'email',
+            type: 'text',
+            autocomplete: 'email',
+            label: 'Your E-mail',
+            rules: 'required|email',
+            messages: {
+              required: 'We need your email! please.',
+              email: 'No no no, give me a real email!'
+            },
+            value: ''
+          },
+          {
+            name: 'password',
+            type: 'password',
+            autocomplete: 'new-password',
+            label: 'Create password',
+            rules: 'required|min:8',
+            messages: {
+              required: 'No password? really?!',
+              'min:8': 'Are you kidding me? give me your real password!'
+            },
+            value: ''
+          }
+        ],
+        this.$axios
+      )
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.form.post('/login').then((response) => alert('Done!'))
+    }
+  }
+}
+</script>
